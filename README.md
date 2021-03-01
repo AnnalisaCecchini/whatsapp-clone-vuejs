3783a017075998d3950ad9674905ac7a5fdced0e
Annalisa Cecchini <annalisa.cecchini@live.it>
2021-03-01 21:11:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+<p align="center">
+    <img src="https://img.shields.io/badge/Vue.js-%5E2.6.12-orange)"/>
+</p>
+
+<h1 align="center">
+    Whatsapp | Clone - with Vue.js
+</h1>
+
+<p align="center">
+    Whatsapp is VoIP service owned by Facebook, Inc. that allows users to send text messages. This is my clone of the Web App Whatsapp.
+</p>
+
+<p align="center">
+    <img src="https://imagizer.imageshack.com/img923/4428/XqWJV0.png" />
+</p>
+
+### :fire: Features
+--------
+
+This is a single page Web App clone: 
+
+:white_check_mark: **Whatsapp Clone**  
+For this page I followed [Whatsapp](https://web.whatsapp.com/) web app design, simulating some of its peculiar features: 
+
+* You can **search for the desired contact**, in the sidebar
+* You can **send message** to the contact you prefer to text with
+* You will also **get an answer** from him/her (after 1 second!)
+* And **last login will be updated** automatically, based on the last message
+
+Try it!
+
+<p align="center">
+  <img src="https://imagizer.imageshack.com/img922/8383/WALIoz.png" alt="whatsapp-add" width="400" />
+</p>
+
+###  :heavy_exclamation_mark: :speech_balloon: Get in Touch  :heavy_exclamation_mark:
+----------
+
+:arrow_forward: Visit my website [annalisacecchini.com](https://annalisacecchini.com)  
+:arrow_forward: Download [my Resume](https://annalisacecchini.com/download)  
+:arrow_forward: Send me an e-mail at [annalisacecchini.dev@gmail.com](mailTo:annalisacecchini.dev@gmail.com)  
+
+&copy; Annalisa Cecchini - 2020
---
+++ js/main.js
@@ -0,0 +1,154 @@
+/**
+ * Boolzapp Vue
+ */
+var app = new Vue({
+    el: '#app',
+    data: {
+        user: {
+            name: 'Annalisa Cecchini',
+            avatar: '_io'
+        },
+        newMessages: '', 
+        indexContact: 0,
+        searchInput: '',
+        contacts: [
+            {
+                name: 'Gustav Purpleson',
+                avatar: '_1',
+                visible: true,
+                lastOnline:'10/01 16:15',
+                messages: [
+                    {
+                        date: '10/01/2021 15:30:55',
+                        message: 'Hi Gustav, have you already informed Silvia about tomorrow\'s meeting?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2021 15:50:00',
+                        message: 'I sent her an invitation on Zoom',
+                        status: 'received'
+                    },
+                    {
+                        date: '10/01/2021 16:15:22',
+                        message: 'Perfect, then see you tomorrow!',
+                        status: 'sent'
+                    }
+                ],
+            },
+            {
+                name: 'Jane Doe',
+                avatar: '_2',
+                visible: true,
+                lastOnline:'20/03 16:35',
+                messages: [
+                    {
+                        date: '20/12/2020 16:30:00',
+                        message: 'Hi Jane, have you talked to the client yet?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '20/12/2020 16:30:55',
+                        message: 'We made an appointment for tomorrow!',
+                        status: 'received'
+                    },
+                    {
+                        date: '20/12/2020 16:35:00',
+                        message: 'Let\'s update tomorrow then!',
+                        status: 'sent'
+                    }
+                ],
+            },
+            {
+                name: 'Sue Shei',
+                avatar: '_3',
+                visible: true,
+                lastOnline: '28/03 16:15',
+                messages: [
+                    {
+                        date: '28/10/2020 10:10:40',
+                        message: 'I\'d love to learn react..',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/10/2020 10:20:10',
+                        message: 'Fantastic, there is a new project in the making, which you can join!',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/10/2020 16:15:22',
+                        message: 'Great, I start immediately then!!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Miles Tone',
+                avatar: '_4',
+                visible: true,
+                lastOnline: '10/01 15:50', 
+                messages: [
+                    {
+                        date: '10/09/2020 15:30:55',
+                        message: 'I would like to evaluate an interesting proposal together',
+                        status: 'received'
+                    },
+                    {
+                        date: '10/09/2020 15:50:00',
+                        message: 'Let\'s meet in half an hour and have coffee together',
+                        status: 'sent'
+                    }
+                ],
+            },
+        ],
+        
+
+    },
+    methods: {
+        setContact(index) {
+            this.indexContact = index;
+        }, 
+        addMessage(index) {
+            if (this.newMessages.trim() !== '') {
+                this.contacts[index].messages.push({
+                    date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
+                    message: this.newMessages,
+                    status: 'sent',
+                });
+
+                this.newMessages = '';
+            }
+
+            setTimeout(() => {
+                this.contacts[index].messages.push({
+                    date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
+                    message: 'Perfect! Let\'s do it now!',
+                    status: 'received',
+                });
+                this.contacts[this.indexContact].lastOnline = dayjs().format('DD/MM HH:mm')
+
+            }, 1000);
+          
+        },
+        lookFor() {
+
+            if (this.searchInput.trim() !== '' ) { 
+                
+                for (var i = 0; i < this.contacts.length; i++) {
+                    
+                    if (! this.contacts[i].name.trim().toLowerCase().includes(this.searchInput.trim().toLowerCase())) {
+                        this.contacts[i].visible = false;
+                    }
+                    else {
+                        this.contacts[i].visible = true;
+                    };
+                };
+            }
+            else if (this.searchInput == '') {
+
+                for (var i = 0; i < this.contacts.length; i++) {
+                    this.contacts[i].visible = true;
+                };
+            };
+        },
+    }
+});
